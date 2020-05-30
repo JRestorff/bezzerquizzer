7f6767f280932ed115e66a6c7b9c07ac9b78caf7
JRestorff <restorff@t-online.de>
2020-05-30 12:48:21 +0200

    Änderungen am JavaScript-Code.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,49 @@
 
 var score = 0
 
+var currentQuestion = 0
+
+var questions = [
+  {
+    question: "Who is the German chancelor?",
+    answer: "Angela Merkel",
+  },
+  {
+    question: "What is the German capital?",
+    answer: "Berlin",
+  },
+  {
+    question: "What is the most beautiful color in the world?",
+    answer: "Blue",
+  },
+]
+
+var numQuestions = questions.length - 1
 
 function askQuestion() {
-  var answer = prompt("Which quiz game is the best?")
-  if (answer == "Bezzerquizzer") {
-    alert("That is correct!")
+  var myCurrentQuestion = questions[currentQuestion].question  
+  var myCurrentAnswer = questions[currentQuestion].answer
+  var answer = prompt(myCurrentQuestion)
+  if (answer == myCurrentAnswer) {    
     score += 1
-    document.getElementById('box-' + score).style.backgroundColor = "red"
+    document.getElementById('box-' + score).style.backgroundColor = "red" 
+    removeRed()
   } else {
     alert("That is wrong, try again!")
   }
-  console.log(score)
+  nextQuestion()
+}
+
+function removeRed() {
+  for (var i = 0; i < score; i++) {
+    document.getElementById('box-' + i).style.backgroundColor = "black"
+  }
+}
+
+function nextQuestion() {
+  if (currentQuestion == numQuestions) {
+    currentQuestion = 0
+  }  else {
+    currentQuestion += 1
+  }
 }
